8c7dd61307e3cbf1a98eb257d3561c4af24d02a5
Muhammad Kuswari <muh.kuswari10@gmail.com>
2020-10-15 06:32:49 +0700

    add some route

--- apps/routes/routes.php
+++ apps/routes/routes.php
@@ -1,10 +1,14 @@
 <?php
-    $routes = [];
-    $true_url = '';
-    Route::get('admin/home/:ad', function ($params) {
-        var_dump($params);
-    });
+$routes = [];
+$true_url = '';
+Route::get('admin/home/:ad', function ($params) {
+    var_dump($params);
+});
 
-    Route::get('', function () {
-        var_dump("ok");
-    });
+Route::get('', function () {
+    var_dump("ok");
+});
+
+Route::get('/about', function () {
+    echo 'About Us';
+});
